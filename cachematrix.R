784265b9ed36df89d5734b21018f72fdc465e476
Dušan Ranđelović <randjelovicdushan@gmail.com>
2015-05-24 21:32:10 +0200

    init, analogy

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching is done analogous to the example. But in esample there is no inspecting for NAs, also in this function it could cause problems. Additionaly, some matrices don-t have inverse.
+## TODO: Inspect if matrix has inverse by finding det(x) != 0
 
-## Write a short comment describing this function
+## Analogous to the example with mean we have:
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) m <<- inverse
+	getinverse <- function() m
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## Caching the inverse: inspecting first to see if cached value exist
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setinverse(m)
+	m
 }
